699e5074bc6a2a525fc6024355bb09b42f83572e
Rob Neuhaus <rrenaud@gmail.com>
2023-03-07 23:46:02 -0500

    Got tests working with indexed berry positions.

--- preprocess.py
+++ preprocess.py
@@ -5,47 +5,14 @@ import dateutil.parser
 import numpy as np
 from typing import List, Optional, Dict
 
+import map_structure
+from constants import Team, GateState, VictoryCondition, PlayerCategory, MaidenType, Map
 
 # https://kqhivemind.com/wiki/Stats_Socket_Events
 
 
-class GateState(Enum):
-    BLUE = 0
-    GOLD = 1
-    NEUTRAL = 2
-
-
-class Team(Enum):
-    BLUE = 0
-    GOLD = 1
-
-
 def opposing_team(team: Team) -> Team:
     return Team.GOLD if team == Team.BLUE else Team.BLUE
-
-
-class VictoryCondition(Enum):
-    military = 'military'
-    economic = 'economic'
-    snail = 'snail'
-
-
-class PlayerCategory(Enum):
-    Queen = 'Queen'
-    Soldier = 'Soldier'
-    Worker = 'Worker'
-
-
-class MaidenType(Enum):
-    maiden_speed = 'maiden_speed'
-    maiden_wings = 'maiden_wings'
-
-
-class Map(Enum):
-    map_day = 'map_day'
-    map_night = 'map_night'
-    map_dusk = 'map_dusk'
-    map_twilight = 'map_twilight'
 
 
 class DroneState:
@@ -58,14 +25,15 @@ class DroneState:
 class TeamState:
     def __init__(self):
         self.eggs = 2
-        self.berries_deposited = 0  # TODO: Better model easy/hard berries, eg on Dusk or Twilight.
+        self.berries_deposited = [False for _ in range(12)]
         self.drones = [DroneState() for _ in range(4)]
 
 
 class GameState:
-    def __init__(self, map_name):
+    def __init__(self, map_info: map_structure.MapStructureInfo):
+        self.map_info = map_info
         self.teams = [TeamState() for _ in range(2)]
-        self.berries_available = 0
+        self.berries_available = map_info.total_berries
         self.gate_states = [GateState.NEUTRAL for _ in range(5)]
 
     def get_team(self, team: Team) -> TeamState:
@@ -108,14 +76,15 @@ class MapStartEvent(GameEvent):
     def __init__(self, payload_values: List[str]):
         super().__init__()
         self.map = Map[payload_values[0]]
+        self.gold_on_left = payload_values[3] == 'True'
         self.game_version = payload_values[4]
 
 
 class BerryDepositEvent(GameEvent):
     def __init__(self, payload_values: List[str]):
         super().__init__()
-        self.drone_x = int(payload_values[0])
-        self.drone_y = int(payload_values[1])
+        self.hole_x = int(payload_values[0])
+        self.hole_y = int(payload_values[1])
         self.position_id = int(payload_values[2])
 
     def modify_game_state(self, game_state: GameState):
@@ -123,7 +92,8 @@ class BerryDepositEvent(GameEvent):
         drone_index = position_id_to_drone_index(self.position_id)
         team_state: TeamState = game_state.get_team(team)
         team_state.drones[drone_index].has_berry = False
-        team_state.berries_deposited += 1
+        berry_index = game_state.map_info.get_berry_index(self.hole_x, self.hole_y)
+        team_state.berries_deposited[berry_index] = True
         game_state.berries_available -= 1
 
 
@@ -140,16 +110,21 @@ class BerryKickInEvent(GameEvent):
         if not self.counts_for_own_team:
             team = opposing_team(team)
 
-        game_state.get_team(team).berries_deposited += 1
+        berry_index = game_state.map_info.get_berry_index(self.hole_x, self.hole_y)
+        game_state.get_team(team).berries_deposited[berry_index] = True
         game_state.berries_available -= 1
 
 
 class BlessMaidenEvent(GameEvent):
     def __init__(self, payload_values: List[str]):
         super().__init__()
-        self.gate_x = int(payload_values[0])
-        self.gate_y = int(payload_values[1])
+        self.maiden_x = int(payload_values[0])
+        self.maiden_y = int(payload_values[1])
         self.gate_color = GateState.BLUE if payload_values[2] == 'Blue' else GateState.GOLD
+
+    def modify_game_state(self, game_state: GameState):
+        team: Team = Team.BLUE if self.gate_color == GateState.BLUE else Team.GOLD
+        # game_state.gate_states
 
 
 class CarryFoodEvent(GameEvent):
@@ -177,6 +152,9 @@ class GetOffSnailEvent(GameEvent):
 class GlanceEvent(GameEvent):
     def __init__(self, payload_values: List[str]):
         super().__init__()
+        if len(payload_values) == 2:
+            # Hack for older versions where the payload was missing the glance coordinates.
+            payload_values = ['-1', '-1'] + payload_values
         self.glance_x = int(payload_values[0])
         self.glance_y = int(payload_values[1])
         self.position_ids = [int(payload_values[2]), int(payload_values[3])]
@@ -211,7 +189,9 @@ class VictoryEvent(GameEvent):
 def parse_event(raw_event_row) -> Optional[GameEvent]:
     skippable_events = {'gameend', 'playernames', 'spawn',
                         'reserveMaiden', 'unreserveMaiden', 'snailEat',
-                        'snailEscape'}
+                        'snailEscape',
+                        'cabinetOnline', 'cabinetOffline',
+                        'bracket', 'tstart', 'tournamentValidation', 'checkIfTournamentRunning'}
     dispatcher = {'berryDeposit': BerryDepositEvent,
                   'berryKickIn': BerryKickInEvent,
                   'carryFood': CarryFoodEvent,
@@ -259,31 +239,46 @@ def debug_print_events(events: GameEvents):
         print(event.__class__.__name__.removesuffix('Event'), attrs)
 
 
-def get_map(events: GameEvents) -> Map:
+def get_map_start(events: GameEvents) -> MapStartEvent:
     for event in events:
-        if hasattr(event, 'map_name'):
-            return event.map_name
-    raise ValueError('No map found in events')
+        if isinstance(event, MapStartEvent):
+            return event
+    raise ValueError('No map start found in events')
 
 
-def vectorize_game_states(events: GameEvents) -> np.ndarray:
-    game_state = GameState(get_map(events))
+def vectorize_game_states(map_structure_infos: map_structure.MapStructureInfos,
+                          events: GameEvents) -> np.ndarray:
+    map_start: MapStartEvent = get_map_start(events)
+    map_info: map_structure.MapStructureInfo = map_structure_infos.get_map_info(map_start.map, map_start.gold_on_left)
+
+    game_state = GameState(map_info)
     for event in events:
         event.modify_game_state(game_state)
 
 
-
-def main():
-    event_reader = csv.DictReader(open('match_data/export_20220928_183155_gdc6/gameevent.csv'))
+def read_events_from_csv(csv_path: str, skip_raw_events_fn) -> GameEvents:
+    event_reader = csv.DictReader(open(csv_path))
     events = []
     for row in event_reader:
-        maybe_event = parse_event(row)
+        try:
+            if skip_raw_events_fn and skip_raw_events_fn(row):
+                continue
+            maybe_event = parse_event(row)
+        except Exception as e:
+            print(f'Failed to parse event: {row}, {e}')
+            continue
         if maybe_event is not None:
             events.append(maybe_event)
+    return events
 
+
+def main():
+    events = read_events_from_csv('match_data/export_20220928_183155_gdc6/gameevent.csv')
     grouped_events = group_events_by_game(events)
     #debug_print_events(next(iter(grouped_events.values())))
-    vectorize_events(next(iter(grouped_events.values())))
+    map_structure_infos = map_structure.MapStructureInfos()
+    for game_id, game_events in grouped_events.items():
+        vectorize_game_states(map_structure_infos, game_events)
 
 if __name__ == '__main__':
     main()
